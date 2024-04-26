e51fef5abbd07067a5a572de9fe8dcc4f6fdfacc
noorirshad <noorirshad708@gmail.com>
2024-04-26 10:45:32 +0500

    Add project files.

---
+++ AuthGateway/Controllers/AuthController.cs
@@ -0,0 +1,104 @@
+﻿using AuthGateway.Model;
+using AuthGateway.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthGateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IConfiguration _configuration;
+        public AuthController(UserManager<ApplicationUser> userManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// add user into the system
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        /// <response code="400">If the item is null</response>
+        [HttpPost]
+        [Route("register-user")]
+        public async Task<IActionResult> RegisterUser(UserRegisterViewModel model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+            if (user != null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseViewModel { Status = "Error", Message = "Email already exists!" });
+            }
+           
+            ApplicationUser applicationUser = new ApplicationUser()
+            {
+                SecurityStamp = Guid.NewGuid().ToString(),
+                UserName = model.Email,
+                Email = model.Email,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                CreatedDateTime = DateTime.Now,
+            };
+            var result = await _userManager.CreateAsync(applicationUser, model.Password);
+            if (result.Succeeded != true)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new ResponseViewModel { Status ="Error", Message = "Account creation failed! Please check the details and try again." });
+            }
+            return Ok(new ResponseViewModel { Status = "Success", Message = "Account create successfully!", Data = applicationUser.Id });
+        }
+        /// <summary>
+        /// Login for  User.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns>User Token, User Id and user role</returns>
+        /// <response code="400">If the item is null</response>       
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user == null)
+            {
+                return BadRequest(new ResponseViewModel { Status = "Error", Message = "The userName is not associated with any account" });
+            }
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                var authClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+
+            };
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+                DateTime tokenExpireDateTime = DateTime.Now.AddHours(3);
+                var token = new JwtSecurityToken(
+                    issuer: _configuration["JWT:ValidIssuer"],
+                    audience: _configuration["JWT:ValidAudience"],
+                    expires: tokenExpireDateTime,
+                    claims: authClaims,
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                    );
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token),
+                    expiration = token.ValidTo,
+                    userId = user.Id
+                });
+            }
+            else
+            {
+                return Unauthorized(new ResponseViewModel { Status = "Error", Message = "The password that you've entered is incorrect" });
+            }
+        }
+    }
+}
---
+++ AuthGateway/Data/ApplicationDBContext.cs
@@ -0,0 +1,38 @@
+﻿using AuthGateway.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace AuthGateway.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+         : base(options)
+        {
+        }
+        public virtual DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>(entity =>
+            {
+                entity.ToTable("ApplicationUser");
+
+
+                entity.Property(e => e.FirstName).HasMaxLength(100);
+
+                entity.Property(e => e.LastName).HasMaxLength(100);
+
+            });
+
+        }
+    }
+}
